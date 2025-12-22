caae2371d7151ebf96a5f1127df6a57514c41931
George Reda <dr.george.r.n@gmail.com>
2025-12-22 23:58:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,8 @@
 const REPO_NAME = "semester-3";
 const GITHUB_USER = "MUE24Med";
 
-const NEW_API_BASE = `https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/contents`;
-const TREE_API_URL = `https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/git/trees/main?recursive=1`;
-const RAW_CONTENT_BASE = `https://raw.githubusercontent.com/${GITHUB_USER}/${REPO_NAME}/main/`;
+const label = rect.parentNode.querySelector(`.rect-label[data-original-for='${rect.dataset.href}']`);
+const bg = rect.parentNode.querySelector(`.label-bg[data-original-for='${rect.dataset.href}']`);
 
 
 let globalFileTree = [];
