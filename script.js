3c1f040d7dea79da89e9c99c0100434615b1e0db
George Reda <dr.george.r.n@gmail.com>
2025-12-23 07:31:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const NEW_API_BASE = `https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/c
 const TREE_API_URL = `https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/git/trees/main?recursive=1`;
 const RAW_CONTENT_BASE = `https://raw.githubusercontent.com/${GITHUB_USER}/${REPO_NAME}/main/`;
 
-let globalFileTree = []; 
-
 // 2. دالة جلب البيانات (التي تجعل الموقع يعمل أوفلاين لاحقاً)
 async function fetchGlobalTree() {
     if (globalFileTree.length > 0) return; 
